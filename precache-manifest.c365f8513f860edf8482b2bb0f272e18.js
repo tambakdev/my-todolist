5e642b0d193067325ce572b5d103ee5deb028f2a
tambakdev <tambakdev@gmail.com>
2020-10-21 05:47:05 +0700

    Updates

---
+++ precache-manifest.c365f8513f860edf8482b2bb0f272e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83960305bb5cee9bc45223976d4100d9",
+    "url": "/my-todolist/index.html"
+  },
+  {
+    "revision": "3e8dbcdb7ab1cfb9ccfc",
+    "url": "/my-todolist/static/css/2.d00bb1db.chunk.css"
+  },
+  {
+    "revision": "3e8dbcdb7ab1cfb9ccfc",
+    "url": "/my-todolist/static/js/2.88f5a4eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-todolist/static/js/2.88f5a4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce6da4c6e71fec289dd",
+    "url": "/my-todolist/static/js/main.8d8d62a1.chunk.js"
+  },
+  {
+    "revision": "92c9d3c5632c702e9554",
+    "url": "/my-todolist/static/js/runtime-main.263feb07.js"
+  }
+]);
